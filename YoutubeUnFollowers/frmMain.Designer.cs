94ff644d447ff2b6f09aeb9b6916631a0cce1d96
Pairs34 <a.yldrm@outlook.com.tr>
2020-06-12 20:45:05 +0300

    AllIsOk-WaitingFullButtom

---
+++ YoutubeUnFollowers/frmMain.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace YoutubeUnFollowers
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnUnfollow = new System.Windows.Forms.Button();
+            this.lstUnfollowers = new System.Windows.Forms.ListBox();
+            this.lblUnfollowers = new System.Windows.Forms.Label();
+            this.txtChannelName = new System.Windows.Forms.TextBox();
+            this.lblChannelName = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnUnfollow
+            // 
+            this.btnUnfollow.Location = new System.Drawing.Point(217, 4);
+            this.btnUnfollow.Name = "btnUnfollow";
+            this.btnUnfollow.Size = new System.Drawing.Size(108, 23);
+            this.btnUnfollow.TabIndex = 1;
+            this.btnUnfollow.Text = "Get Unfollowers";
+            this.btnUnfollow.UseVisualStyleBackColor = true;
+            this.btnUnfollow.Click += new System.EventHandler(this.btnUnfollow_Click);
+            // 
+            // lstUnfollowers
+            // 
+            this.lstUnfollowers.FormattingEnabled = true;
+            this.lstUnfollowers.Location = new System.Drawing.Point(13, 66);
+            this.lstUnfollowers.Name = "lstUnfollowers";
+            this.lstUnfollowers.Size = new System.Drawing.Size(312, 251);
+            this.lstUnfollowers.TabIndex = 2;
+            // 
+            // lblUnfollowers
+            // 
+            this.lblUnfollowers.AutoSize = true;
+            this.lblUnfollowers.Location = new System.Drawing.Point(13, 43);
+            this.lblUnfollowers.Name = "lblUnfollowers";
+            this.lblUnfollowers.Size = new System.Drawing.Size(119, 13);
+            this.lblUnfollowers.TabIndex = 3;
+            this.lblUnfollowers.Text = "Takip Etmeyen Kanallar";
+            // 
+            // txtChannelName
+            // 
+            this.txtChannelName.Location = new System.Drawing.Point(71, 6);
+            this.txtChannelName.Name = "txtChannelName";
+            this.txtChannelName.Size = new System.Drawing.Size(140, 20);
+            this.txtChannelName.TabIndex = 4;
+            // 
+            // lblChannelName
+            // 
+            this.lblChannelName.AutoSize = true;
+            this.lblChannelName.Location = new System.Drawing.Point(13, 9);
+            this.lblChannelName.Name = "lblChannelName";
+            this.lblChannelName.Size = new System.Drawing.Size(52, 13);
+            this.lblChannelName.TabIndex = 5;
+            this.lblChannelName.Text = "Kanal Adı";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(337, 327);
+            this.Controls.Add(this.lblChannelName);
+            this.Controls.Add(this.txtChannelName);
+            this.Controls.Add(this.lblUnfollowers);
+            this.Controls.Add(this.lstUnfollowers);
+            this.Controls.Add(this.btnUnfollow);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmMain";
+            this.Text = "YT Unfollowers";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnUnfollow;
+        private System.Windows.Forms.ListBox lstUnfollowers;
+        private System.Windows.Forms.Label lblUnfollowers;
+        private System.Windows.Forms.TextBox txtChannelName;
+        private System.Windows.Forms.Label lblChannelName;
+    }
+}
+
---
+++ YoutubeUnFollowers/frmMain.cs
@@ -0,0 +1,142 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using DevExpress.Utils.Extensions;
+using System.Collections.Generic;
+using OpenQA.Selenium.Support.UI;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace YoutubeUnFollowers
+{
+    public partial class frmMain : Form
+    {
+        IWebDriver driver;
+        const string FOLLOWERS = "https://www.youtube.com/feed/channels";
+        public frmMain()
+        {
+            InitializeComponent();
+            Process.GetProcesses()
+                     .Where(pr => pr.ProcessName.Contains("gecko") || pr.ProcessName.Contains("firefox"))
+                     .ForEach(p => p.Kill());
+            var driverService = FirefoxDriverService.CreateDefaultService();
+            driverService.HideCommandPromptWindow = true;
+
+            string pathToCurrentUserProfiles = Environment.ExpandEnvironmentVariables("%APPDATA%") + @"\Mozilla\Firefox\Profiles";
+            string[] pathsToProfiles = Directory.GetDirectories(pathToCurrentUserProfiles, $"*.default", SearchOption.TopDirectoryOnly);
+            string profilePath = string.Empty;
+            if (pathsToProfiles.Length != 0)
+            {
+                profilePath = pathsToProfiles[0];
+            }
+
+            FirefoxProfile ffProfile = new FirefoxProfile(profilePath);
+
+            driver = new FirefoxDriver(driverService,new FirefoxOptions() { Profile = ffProfile });
+            driver.Url = FOLLOWERS;
+        }
+
+        public double GetScrollPosition(IWebDriver driver)
+        {
+            IJavaScriptExecutor js = (IJavaScriptExecutor)driver;
+            var value = js.ExecuteScript("return window.scrollHeight;");
+            return Convert.ToDouble(value);
+        }
+
+        public double OldPosition = 0;
+        public int attempt = 0;
+        public void GotoFullButtom()
+        {
+            if (attempt == 30)
+            {
+                return;
+            }
+
+            var scPosition = GetScrollPosition(driver);
+
+            IJavaScriptExecutor js = (IJavaScriptExecutor)driver;
+            js.ExecuteScript("window.scrollTo([0], 1000)", scPosition);
+
+            Thread.Sleep(2000);
+            attempt++;
+            GotoFullButtom();
+        }
+
+        protected void WaitForPageLoad()
+        {
+            try
+            {
+                IJavaScriptExecutor javaScriptExecutor = (IJavaScriptExecutor)driver;
+                WebDriverWait webDriverWait = new WebDriverWait(driver, new TimeSpan(0, 0, 15));
+                webDriverWait.Until<bool>((IWebDriver wd) => javaScriptExecutor.ExecuteScript("return document.readyState").Equals("complete"));
+            }
+            catch (Exception) { }
+        }
+
+        List<string> GetLinks()
+        { 
+            List<string> links = new List<string>();
+            var elements = driver.FindElements(By.Id("main-link"));
+            foreach (var item in elements)
+            {
+                links.Add(item.GetAttribute("href"));
+            }
+
+            return links;
+        }
+
+        List<string> CheckIsFollow(List<string> links)
+        {
+            List<string> unFollowers = new List<string>();
+            foreach (var item in links)
+            {
+                //string url = "https://www.youtube.com/user/ExtremitySoft/channels";
+                driver.Navigate().GoToUrl(item + "/channels");
+
+                WaitForPageLoad();
+
+                GotoFullButtom();
+
+                try
+                {
+                    var channels = driver.FindElements(By.XPath("//span[@id='title']"));
+
+                    if (channels.Count == 0)
+                    {
+                        unFollowers.Add(item);
+                    }
+                    else
+                    {
+                        var unFollowerChannels = channels.Where(x => x.Text == txtChannelName.Text).ToList();
+                        if (unFollowerChannels.Count == 0)
+                        {
+                            unFollowers.Add(item);
+                        }
+                    }
+                }
+                catch (Exception)
+                {
+                    unFollowers.Add(item);
+                }
+
+                break;
+            }
+            return unFollowers;
+        }
+
+        private void btnUnfollow_Click(object sender, EventArgs e)
+        {
+            //main-link
+
+            var links = GetLinks();
+
+            var unFollowers = CheckIsFollow(links);
+
+            lstUnfollowers.Items.AddRange(unFollowers.ToArray());
+        }
+    }
+}
